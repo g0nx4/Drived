f73b9de26d8b6433a555593d944a4ecd1311630d
g0nxz4 <143147491+g0nxz4@users.noreply.github.com>
2023-12-31 15:04:45 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+#!/bin/python
+
+import json
+import requests
+import time
+logo=("""
+________       _____            _________
+___  __ \_________(_)__   ____________  /
+__  / / /_  ___/_  /__ | / /  _ \  __  / 
+_  /_/ /_  /   _  / __ |/ //  __/ /_/ /  
+/_____/ /_/    /_/  _____/ \___/\__,_/   
+
+by: _g0nx4
+""")
+print(logo)
+placa=input("Matricula: ")
+if len(placa)<6 or len(placa)>6:
+	print("placa no valida")
+	exit()
+else:
+	pass
+url = (f"https://api.boostr.cl/vehicle/{placa}.json")
+config = {"accept": "application/json"}
+
+response = requests.get(url,headers=config)
+
+multas_url=(f"https://api.boostr.cl/vehicle/traffic_tickets/{placa}.json")
+mu_res=requests.get(multas_url,headers=config)
+mul=json.loads(mu_res.text)
+r=json.loads(response.text)
+#print(response_json)
+time.sleep(0.3)
+print(f"Dueño: {r['data']['owner']['fullname']}")
+time.sleep(0.3)
+print(f"Rut: {r['data']['owner']['documentNumber']}")
+time.sleep(0.3)
+print(f"Matricula: {r['data']['plate']}")
+time.sleep(0.3)
+print(f"Multas: {mul['data']['tickets']}")
+print(f"Matricula Full: {r['data']['plate']}-{r['data']['dv']}") 
+time.sleep(0.3)
+print(f"Marca: {r['data']['make']}")
+time.sleep(0.3)
+print(f"Modelo: {r['data']['model']}")
+time.sleep(0.3)
+print(f"Año: {r['data']['year']}")
+time.sleep(0.3)
+print(f"Tipo de Auto: {r['data']['type']}")
+time.sleep(0.3)
+print(f"Motor: {r['data']['engine']}")
+
+
+
+#print(response_json["data"]["year"])
+#print(response_json["data"]["owner"]["fullname"])
+
